fe7353ac97e14666ff756e51a9c290755df609f9
Arif Budiman <arifbdmn69@gmail.com>
2022-04-27 20:04:32 +0700

    fix: Handler User

--- main.go
+++ main.go
@@ -24,40 +24,7 @@ var (
 	authService service.AuthService = service.NewAuthService(userRepo)
 	jwtService  service.JWTService  = service.NewJWTService()
 	userService service.UserService = service.NewUserService(userRepo)
-	// authHandler auth.AuthController = *auth.NewAuthController(authService, jwtService, userService)
-	// userHandler user.UserController = *user.NewUserController(userService, jwtService)
 )
-
-// type Controller struct {
-// 	Auth *auth.AuthController
-// 	User *user.UserController
-// }
-
-// func RegisterRoutes(e *echo.Echo, controller *Controller) {
-
-// 	authRoutes := e.Group("/api/v1/auth")
-// 	authRoutes.POST("/login", controller.Auth.Login)
-// 	authRoutes.POST("/register", controller.Auth.Register)
-
-// 	userRoutes := e.Group("/api/v1/user", middleware.AuthorizeJWT(jwtService))
-// 	userRoutes.Use(middleware.AuthorizeJWT(service.NewJWTService()))
-// 	userRoutes.GET("/profile", controller.User.Profile)
-// 	userRoutes.PUT("/profile", controller.User.Update)
-
-// }
-
-// func main() {
-// 	defer config.CloseDatabaseConnection(db)
-
-// 	e := echo.New()
-// 	controller := &Controller{
-// 		Auth: auth.NewAuthController(authService, jwtService, userService),
-// 		User: user.NewUserController(userService, jwtService),
-// 	}
-// 	RegisterRoutes(e, controller)
-
-// 	e.Logger.Fatal(e.Start(":8081"))
-// }
 
 func main() {
 	defer config.CloseDatabaseConnection(db)
